14854e43599cf795778de531aab75f90b0ce6e83
ggroshansii <garthgroshansii@gmail.com>
2021-10-18 13:57:27 -0400

    added pillow and Profile model in accounts app models

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 
 class CustomUser(AbstractUser):
     pass
 
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='profiles/', null=True)
+    display_name = models.CharField(max_length = 255, null=True)
 
+    def __str__(self):
+        return self.user.username
