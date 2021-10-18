002bf0bd0dae20cfafcc058d213f49c3765a8666
ggroshansii <garthgroshansii@gmail.com>
2021-10-18 14:35:42 -0400

    in articles app, created serializers.pywith ArticleSerializer, a ListView with Article model and url path

--- articles/models.py
+++ articles/models.py
@@ -1,9 +1,12 @@
 
 from django.db import models
+from django.conf import settings
 
 class Article(models.Model):
     author = models.CharField(max_length = 255)
+    title = models.CharField(max_length= 255)
     body = models.CharField(max_length= 255)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
-    
+    image = models.ImageField(upload_to='profiles/', null=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.cascade)
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import ArticleSerializer
+from .models import Article
 
 # Create your views here.
+
+class ArticleListView (generics.ListAPIView):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+
+    
