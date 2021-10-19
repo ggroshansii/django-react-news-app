f377fa030a884c81d348c687759f72d076433647
ggroshansii <garthgroshansii@gmail.com>
2021-10-18 20:56:55 -0400

    styled categories div, date and name

--- frontend/static/src/components/Header/Header.js
+++ frontend/static/src/components/Header/Header.js
@@ -11,49 +11,55 @@ export default function Header() {
 
     let month_string;
     switch (month + 1) {
-        case "1":
+        case 1:
             month_string = "January";
             break;
-        case "2":
+        case 2:
             month_string = "February";
             break;
-        case "3":
+        case 3:
             month_string = "March";
             break;
-        case "4":
+        case 4:
             month_string = "April";
             break;
-        case "5":
+        case 5:
             month_string = "May";
             break;
-        case "6":
+        case 6:
             month_string = "June";
             break;
-        case "7":
+        case 7:
             month_string = "July";
             break;
-        case "8":
+        case 8:
             month_string = "August";
             break;
-        case "9":
+        case 9:
             month_string = "September";
             break;
-        case "10":
+        case 10:
             month_string = "October";
             break;
-        case "11":
+        case 11:
             month_string = "November";
             break;
-        case "12":
+        case 12:
             month_string = "December";
             break;
     }
+
+    console.log(month_string)
     return (
         <div className="header-container">
             <h1 className="newspaper-name">The Gartholomew Gazette</h1>
-            <h2>
-                {month_string}/{day}/{year}{" "}
+            <h2 className="header-date">
+                {month_string}, {day} {year}{" "}
             </h2>
+            <div className="header-category-container">
+            <p className="header-category">Categories here....</p>
+            </div>
+
         </div>
     );
 }
