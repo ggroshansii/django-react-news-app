2faedf147abe54ec5ca77aab0bbc4b598e2abdb8
ggroshansii <garthgroshansii@gmail.com>
2021-10-20 10:48:26 -0400

    added author as foreign key to Article model

--- articles/models.py
+++ articles/models.py
@@ -5,10 +5,10 @@ from django.conf import settings
 class Article(models.Model):
     title = models.CharField(max_length= 255)
     body = models.CharField(max_length= 1000)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='profiles/', null=True)
-    # user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     category = models.CharField(max_length = 25)
 
     def __str__(self):
